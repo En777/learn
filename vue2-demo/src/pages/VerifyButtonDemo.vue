05960b1e06206007993733edf9bae331e52ae281
En777 <En777>
2018-10-03 15:42:14 +0800

    VerifyButtonDemo

---
+++ vue2-demo/src/pages/VerifyButtonDemo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="text">
+    <VerifyButton :send="sendSms"></VerifyButton>
+  </div>
+</template>
+
+<script>
+import VerifyButton from '@/components/VerifyButton'
+
+const delay = require('delay')
+
+export default {
+  components: {
+    VerifyButton
+  },
+  methods: {
+    sendSms () {
+      // ajax
+      return delay(500)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
